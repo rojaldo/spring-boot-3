aa218a172602329be2025311231a5fbaa1808b01
Daniel Rojo <danielrojo@gmail.com>
2023-09-19 19:04:03 +0200

    calculator endpoint added

--- src/main/java/com/example/demo/Greeting.java
+++ src/main/java/com/example/demo/Greeting.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Greeting {
+
+    @Autowired
+    private CalculatorService calculatorService;
 
     @GetMapping("/greeting")
     public String greet(Model view) {
@@ -16,11 +20,24 @@ public class Greeting {
 
     @GetMapping("/echo")
     public String echo(
-        @RequestParam(name="msg", required=false, defaultValue="Hello World") String message,
-        Model view) {
+            @RequestParam(name = "msg", required = false, defaultValue = "Hello World") String message,
+            Model view) {
         view.addAttribute("message", message);
         return "greeting_template";
     }
+
+    @GetMapping("/calculator")
+    public String calculator(
+            @RequestParam(name = "op1", required = false, defaultValue = "0") float op1,
+            @RequestParam(name = "op2", required = false, defaultValue = "0") float op2,
+            @RequestParam(name = "operator", required = false, defaultValue = "+") String operator,
+            Model view) {
+        float result = this.calculatorService.calculate(op1, op2, operator);
+        String message = op1 + " " + operator + " " + op2 + " = " + result;
+        view.addAttribute("message", message);
+        return "calculator_template";
+    }
+
 
 
 }
