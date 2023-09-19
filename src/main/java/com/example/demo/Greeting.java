116171bbf1b8cd7c6c7b56079b31e209144629a9
Daniel Rojo <danielrojo@gmail.com>
2023-09-19 16:04:23 +0200

    queryparam example

--- src/main/java/com/example/demo/Greeting.java
+++ src/main/java/com/example/demo/Greeting.java
@@ -1,19 +1,26 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Greeting {
 
     @GetMapping("/greeting")
-    public String greet() {
+    public String greet(Model view) {
+        view.addAttribute("message", "Hello World");
         return "greeting_template";
     }
 
-    @GetMapping("/hello")
-    public String greet2() {
+    @GetMapping("/echo")
+    public String echo(
+        @RequestParam(name="msg", required=false, defaultValue="Hello World") String message,
+        Model view) {
+        view.addAttribute("message", message);
         return "greeting_template";
     }
+
 
 }
